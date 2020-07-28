e9a6646800badcd3554c0b90e0d349f0f5de6906
ProudBloom <jakub-sztompka@wp.pl>
2020-07-28 17:55:21 +0200

    Navbar now completed. Page not responsive yet

--- src/script.js
+++ src/script.js
@@ -1,4 +1,22 @@
 let navButtons = Array.from(document.querySelectorAll('.navigation-button'));
+const colors = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
+const container = document.querySelector('.container');
+
+document.querySelector('.open-navbar-icon').addEventListener('click', () => 
+{
+    container.classList.add('change');
+})
+
+document.querySelector('.close-navbar-icon').addEventListener('click', () => 
+{
+    container.classList.remove('change');
+})
+
+let i = 0;
+
+Array.from(document.querySelectorAll('.nav-link')).forEach( (item) => {
+    item.style.cssText = `background-color: ${colors[i++]}`
+})
 
 navButtons.forEach( (item) =>
 {
@@ -7,3 +25,4 @@ navButtons.forEach( (item) =>
         item.parentElement.parentElement.classList.toggle('change');
     }
 });
+
